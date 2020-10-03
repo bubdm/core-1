cde5730050ea827a5539106742a94d9936cadae4
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-10-03 10:35:24 +0300

    Кнопка с командой

--- WpfApp1/ViewModels/MainWindowViewModel.cs
+++ WpfApp1/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using WpfApp1.Infrastructure.Commands;
 using WpfApp1.ViewModels.Base;
 
 namespace WpfApp1.ViewModels
@@ -8,11 +11,20 @@ namespace WpfApp1.ViewModels
     class MainWindowViewModel : ViewModel
     {
         private string _title = "Главное окно программы";
-
         public string Title
         {
             get => _title;
             set => Set(ref _title, value);
         }
+        #region Команды
+        private ICommand _showDialogCommand;
+        public ICommand ShowDialogCommand 
+            => _showDialogCommand ??= new LambdaCommand(OnShowDialogCommandExecuted);
+        private void OnShowDialogCommandExecuted(object p)
+        {
+            var message = p as string ?? "Hello World!";
+            MessageBox.Show(message, "Сообщение от тестовой команды");
+        }
+        #endregion
     }
 }

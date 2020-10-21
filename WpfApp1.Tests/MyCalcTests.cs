f9bd0a23ee7b2bebbeccff287cb2c153205b672b
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-10-21 09:00:57 +0300

    first test

---
+++ WpfApp1.Tests/MyCalcTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace WpfApp1.Tests
+{
+    [TestClass]
+    public class MyCalcTests
+    {
+        private MyCalc myCalc;
+        private int item;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Debug.WriteLine("Test Init");
+            item = 10;
+            myCalc = new MyCalc();
+            myCalc.Add(item);
+        }
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            Debug.WriteLine("TestStop");
+            myCalc.Dispose();
+        }
+        [TestMethod]
+        public void Check_Constains_Item_In_Items()
+        {
+            CollectionAssert.Contains(myCalc.Items, item);
+        }
+        [TestMethod]
+        public void Check_Remove_Item_From_Items()
+        {
+            myCalc.Remove(0);
+            CollectionAssert.DoesNotContain(myCalc.Items, item);
+        }
+        [TestMethod]
+        public void Sum_10_20_equal_30()
+        {
+            int x = 10;
+            int y = 20;
+            int expected = 30;
+
+            
+            int actual = myCalc.Sum(x, y);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ WpfApp1/MyCalc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp1
+{
+    [Serializable]
+    public class MyCalc : IDisposable
+    {
+        public List<int> Items = new List<int>();
+        public void Add(int item)
+        {
+            Items.Add(item);
+        }
+        public void Remove(int index)
+        {
+            Items.RemoveAt(index);
+        }
+        public int Count { get => Items.Count; }
+        public void Dispose() { }
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}

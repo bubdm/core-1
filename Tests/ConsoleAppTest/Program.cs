58c7ecd59c442359288c9ca8bfb88722301ece5c
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-02-23 21:05:24 +0400

    Подготовка

--- Tests/ConsoleAppTest/Program.cs
+++ Tests/ConsoleAppTest/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Net;
-using System.Net.Mail;
 
 namespace ConsoleAppTest
 {
@@ -8,29 +6,11 @@ namespace ConsoleAppTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            var from = new MailAddress("kanadeiar@gmail.com", "Отправитель");
-            var to = new MailAddress("kanadei@mail.ru", "Получатель");
+            Console.WriteLine("Test Program");
 
-            using var message = new MailMessage(from, to)
-            {
-                Subject = "Заголовок",
-                Body = $"Текст письма {DateTime.Now}",
-            };
-
-            using (var client = new SmtpClient("smtp.gmail.com", 587))
-            {
-                string login = "kanadeiar@gmail.com";
-                Console.Write("Please input e-mail password 'kanadeiar@gmail.com' :>");
-                string password = Console.ReadLine();
-
-                client.EnableSsl = true;
-                client.Credentials = new NetworkCredential(login, password);
-                client.Send(message);
-            }
-            Console.WriteLine("Почта отправлена!");
 
             Console.ReadKey();
         }
     }
+
 }

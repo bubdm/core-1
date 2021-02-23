c6fb66b144ffe8ed2f43a7bc2b01ff59284c79e2
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-02-23 21:00:18 +0400

    Подготовка приложения к net 5

---
+++ WpfApp1/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp1.ViewModels.Base
+{
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public bool Set<T>(ref T _field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(_field, value)) return false;
+            _field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
---
+++ WpfApp1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using WpfApp1.ViewModels.Base;
+
+namespace WpfApp1.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        public MainWindowViewModel()
+        {
+        }
+        private string _title = "Тестовое приложение";
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                Set(ref _title, value);
+            }
+        }
+
+    }
+}

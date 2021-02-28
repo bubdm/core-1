5fee439c6bafc13cf1231cfbc17b5d5cb0a16d79
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-02-28 20:22:29 +0400

    Потоки.

---
+++ Tests/ConsoleAppTest/ThreadsTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace ConsoleAppTest
+{
+    class ThreadsTest
+    {
+        public static void Start()
+        {
+            var messages = Enumerable.Range(1, 100).Select(m => $"Message{m}").ToArray();
+
+
+            
+            for (int i = 0; i < messages.Length - 1; i++)
+            {
+                var i0 = i;
+                ThreadPool.QueueUserWorkItem(_ => { PrintMessage(messages[i0]); });
+            }
+        }
+
+        private static void PrintMessage(string message)
+        {
+            Console.WriteLine($"id:{Thread.CurrentThread.ManagedThreadId} Mes:{message}");
+            Thread.Sleep(3);
+        }
+    }
+}
---
+++ Tests/ConsoleAppTest/ThreadsTesting.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest
+{
+    class Threads
+    {
+
+        public static void Start()
+        {
+            var thread = new Thread(ThreadMethod);
+            thread.Priority = ThreadPriority.Highest;
+            thread.Name = "Clock Thread";
+            thread.Start();
+
+            var message = "Мое сообщение";
+
+            var thread2 = new Thread(() => Printer("новое")) {Priority = ThreadPriority.Lowest};
+            thread2.Start();
+
+            new Thread(PrintObjInfo){Priority = ThreadPriority.Highest}.Start(message);
+
+            var printerTask = new PrintTask(500, "123");
+            printerTask.Start();
+
+            thread2.Join();
+            //if (!thread2.Join(1000))
+            //    thread2.Interrupt();
+        }
+
+        private static void ThreadMethod()
+        {
+            while (true)
+            {
+                Console.Title = DateTime.Now.ToString("HH:mm:ss:ffff");
+                Thread.Sleep(10);
+            }
+        }
+        class PrintTask
+        {
+            private readonly int _id;
+            private readonly string _message;
+            private readonly Thread _Thread;
+
+            public PrintTask(int id, string Message)
+            {
+                _id = id;
+                _message = Message;
+                _Thread = new Thread(ThreadMethod);
+            }
+            private void ThreadMethod()
+            {
+                for (int i = 0; i < 200; i++)
+                {
+                    Console.WriteLine($"Mid:{_id} -- {_message}");
+                    Thread.Sleep(10);
+                }
+            }
+            public void Start()
+            {
+                _Thread.Start();
+            }
+        }
+
+        private static void PrintThreadInfo()
+        {
+            var thread = Thread.CurrentThread;
+            Console.WriteLine($"id {thread.ManagedThreadId} name {thread.Name} priority {thread.Priority} ");
+        }
+
+        private static void Printer(string Message)
+        {
+            PrintThreadInfo();
+
+            var threadid = Thread.CurrentThread.ManagedThreadId;
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine($"id: {threadid} Mes: {Message}");
+                Thread.Sleep(10);
+            }
+        }
+
+        private static void PrintObjInfo(object obj)
+        {
+            PrintThreadInfo();
+
+            var Message = (string) obj;
+
+            var threadid = Thread.CurrentThread.ManagedThreadId;
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine($"id: {threadid} Mes: {Message}");
+                Thread.Sleep(10);
+            }
+        }
+    }
+}

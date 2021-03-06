6fbf968f868bd2dad6d2fd79fb9e01a880021c46
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-03-06 04:20:32 +0400

    Изменения

--- Tests/ConsoleAppTest/Program.cs
+++ Tests/ConsoleAppTest/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using System.Threading;
 
 namespace ConsoleAppTest
 {
@@ -10,7 +9,7 @@ namespace ConsoleAppTest
         {
             Console.WriteLine("Работа программы");
 
-            ThreadManagement.Start();
+            
 
 
 
@@ -19,60 +18,6 @@ namespace ConsoleAppTest
 
             Console.WriteLine("Окончание работы программы. Нажмите кнопку");
             Console.ReadKey();
-        }
-    }
-
-    class ThreadManagement
-    {
-        public static void Start()
-        {
-            var threads = new Thread[10];
-            var auto_reset_event = new AutoResetEvent(false);
-            var manual_reset_event = new ManualResetEvent(false);
-
-            for (int i = 0; i < threads.Length; i++)
-            {
-                var i0 = i;
-                threads[i] = new Thread(() => Printer($"Сообщение {i0}", auto_reset_event));
-                threads[i].Start();
-            }
-
-            Console.WriteLine("Потоки создаты. Старт?");
-
-            
-
-            Console.ReadKey();
-            auto_reset_event.Set();
-
-            Console.ReadKey();
-            auto_reset_event.Set();
-
-            Console.ReadKey();
-            auto_reset_event.Set();
-
-            Console.ReadKey();
-            auto_reset_event.Set();
-
-            
-
-            Console.WriteLine("Завершено выполнение программы.");
-            Console.ReadKey();
-        }
-
-        private static void Printer(string Message, EventWaitHandle EventWait)
-        {
-            var th_id = Thread.CurrentThread.ManagedThreadId;
-            Console.WriteLine($"Поток id:{th_id} начал наботу");
-
-            EventWait.WaitOne();
-
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine($"Id: {th_id}\t{Message}");
-                Thread.Sleep(10);
-            }
-
-            Console.WriteLine($"Поток id:{th_id} завершил работу");
         }
     }
 }

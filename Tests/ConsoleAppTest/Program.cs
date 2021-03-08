b5fa4b3830d9867c0de367cfe05c1c9055a269e7
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-03-08 19:35:55 +0400

    Чистка проекта перед экспериментами с базой данных

--- Tests/ConsoleAppTest/Program.cs
+++ Tests/ConsoleAppTest/Program.cs
@@ -14,34 +14,8 @@ namespace ConsoleAppTest
             SetConsoleCP(65001);        //установка кодовой страницы utf-8 (Unicode) для вводного потока
             SetConsoleOutputCP(65001);  //установка кодовой страницы utf-8 (Unicode) для выводного потока
 
-            //Test_TPL.TestOldVariantTask();
-            //Test_TPL.TestTPLFunctions();
 
-
-            
-            var task = Tasks.TestGoodAsync();
-            Console.WriteLine($"ВНИМАНИЕ! Поток: {Thread.CurrentThread.ManagedThreadId} Начинаем что нить делать тут важное - например обновлять интерфейс");
-            Thread.Sleep(500);
-            Console.WriteLine($"ВНИМАНИЕ! Поток: {Thread.CurrentThread.ManagedThreadId} Задания начали или не начали выполняться асинхронно!?!?!?");
-            await task;
-
-
-            //var val = 0;
-            //while (!task.IsCompleted)
-            //{
-            //    val++;
-            //    Console.Title = $"Задания выполняется {val} секунд";
-            //    Thread.Sleep(1000);
-            //}
-            //if (task.IsCompletedSuccessfully)
-            //    Console.Title = $"Поток: {Thread.CurrentThread.ManagedThreadId} Задача выполнена успешно";
-            //if (task.IsFaulted)
-            //    Console.Title = "Ошибка в выполнении задачи";
-            //if (task.IsCanceled)
-            //    Console.Title = "Задача отменена";
-
-
-            Console.WriteLine($"Поток: {Thread.CurrentThread.ManagedThreadId} Главный поток завершил работу свою");
+            Console.WriteLine("Нажмите любую кнопку ...");
             Console.ReadKey();
         }
     }

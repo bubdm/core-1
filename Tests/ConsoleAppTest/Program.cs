288abe39600d798cd6486d35217b44ac45bcb82b
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-03-14 22:15:05 +0400

    рефлексия

--- Tests/ConsoleAppTest/Program.cs
+++ Tests/ConsoleAppTest/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
@@ -16,7 +19,22 @@ namespace ConsoleAppTest
             SetConsoleOutputCP(65001);  //установка кодовой страницы utf-8 (Unicode) для выводного потока
             #endregion
 
+            const string file = "TestLibrary.dll";
+            Assembly lib = Assembly.LoadFile(Path.GetFullPath(file)); //полная загрузка
+            //Assembly thisAssembly = Assembly.GetEntryAssembly(); //загрузить сам себя
+            var asm2 = typeof(Program).Assembly;
+            var path = asm2.Location;
+
+            var type = lib.DefinedTypes.First();
+
+            var props = type.GetProperties();
+            var mets = type.GetMethods();
+            var ctors = type.GetConstructors();
+            var evs = type.GetEvents();
+            var fiels = type.GetFields();
+
             
+
             Console.WriteLine("Нажмите любую кнопку ...");
             Console.ReadKey();
         }
---
+++ Tests/TestLibrary/InternalPrinter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace TestLibrary
+{
+    [Description("Скрытый принтер")]
+    internal class InternalPrinter : Printer
+    {
+        public int Value { get; private set; } = 5;
+        public InternalPrinter() : base("internal:")
+        {
+        }
+        public override void Print(string Message)
+        {
+            Console.WriteLine("Private print = {0}", Value++);
+            base.Print(Message);
+        }
+    }
+}

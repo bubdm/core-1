e084db0b79f0d5e928ff3237d83b63adffdc7689
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-05-22 07:29:09 +0400

    Контроллер +

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _Configuration;
+        public HomeController(IConfiguration Configuration)
+        {
+            _Configuration = Configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return Content($"Hello => {_Configuration["Hello"]}");
+        }
+
+        public IActionResult Second()
+        {
+            return Content("Second");
+        }
+
+        public IActionResult Third()
+        {
+            return Content($"Third + {_Configuration["Hello"]}");
+        }
+    }
+}

726d8b11820d8c9a6a8c997eaf1bf3c91ed84c68
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-05-24 04:12:43 +0400

    Библиотека доступа к данным создана

---
+++ StudentsDAL.TestDriver/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace StudentsDAL.TestDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleToRussian();
+            Console.WriteLine("Тестирование библиотеки доступа к данным");
+
+
+            Console.WriteLine("Нажмите любую кнопку ...");
+            Console.ReadKey();
+        }
+        private static void ConsoleToRussian()
+        {
+            [DllImport("kernel32.dll")] static extern bool SetConsoleCP(uint pagenum);
+            [DllImport("kernel32.dll")] static extern bool SetConsoleOutputCP(uint pagenum);
+            SetConsoleCP(65001);        //установка кодовой страницы utf-8 (Unicode) для вводного потока
+            SetConsoleOutputCP(65001);  //установка кодовой страницы utf-8 (Unicode) для выводного потока
+        }
+    }
+}

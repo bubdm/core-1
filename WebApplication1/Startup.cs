ca82a18a132d134d51e0566f92333ed38b6d2714
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-05-30 08:03:33 +0400

    разметка главной страницы Index

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -27,6 +27,8 @@ namespace WebApplication1
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>

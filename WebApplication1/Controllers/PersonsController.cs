1179c07925f6bbff1d330d00a07f0b536211d20f
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-06-05 08:56:36 +0400

    Маршруты индивидуальные для контроллера

--- WebApplication1/Controllers/PersonsController.cs
+++ WebApplication1/Controllers/PersonsController.cs
@@ -7,15 +7,18 @@ using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
+    [Route("Staff")]
     public class PersonsController : Controller
     {
         private readonly IEnumerable<Person> _Persons = Person.GetPersons;
 
+        [Route("All")]
         public IActionResult Index()
         {
             return View(_Persons);
         }
 
+        [Route("Info/{id}")]
         public IActionResult Details(int id)
         {
             var person = _Persons.FirstOrDefault(p => p.Id == id);

c00a1c5a542b2e0a0b42f0beee33bd15375285da
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-06-07 20:24:27 +0400

    Каталог товаров начал работать

---
+++ WebApplication1/Controllers/CatalogController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Services.Interfaces;
+
+namespace WebApplication1.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly IProductData _productData;
+        public CatalogController(IProductData productData)
+        {
+            _productData = productData;
+        }
+        public IActionResult Index(int? brandId, int? catalogId)
+        {
+            return View();
+        }
+    }
+}
--- WebApplication1/Services/InMemoryProductData.cs
+++ WebApplication1/Services/InMemoryProductData.cs
@@ -10,38 +10,40 @@ namespace WebApplication1.Services
 {
     public class InMemoryProductData : IProductData
     {
+        #region Стремные данные без базы данных
+
         private readonly IEnumerable<Section> _Sections = new[]
         {
-              new Section { Id = 1, Name = "Спорт", Order = 0 },
-              new Section { Id = 2, Name = "Nike", Order = 0, ParentId = 1 },
-              new Section { Id = 3, Name = "Under Armour", Order = 1, ParentId = 1 },
-              new Section { Id = 4, Name = "Adidas", Order = 2, ParentId = 1 },
-              new Section { Id = 5, Name = "Puma", Order = 3, ParentId = 1 },
-              new Section { Id = 6, Name = "ASICS", Order = 4, ParentId = 1 },
-              new Section { Id = 7, Name = "Для мужчин", Order = 1 },
-              new Section { Id = 8, Name = "Fendi", Order = 0, ParentId = 7 },
-              new Section { Id = 9, Name = "Guess", Order = 1, ParentId = 7 },
-              new Section { Id = 10, Name = "Valentino", Order = 2, ParentId = 7 },
-              new Section { Id = 11, Name = "Диор", Order = 3, ParentId = 7 },
-              new Section { Id = 12, Name = "Версачи", Order = 4, ParentId = 7 },
-              new Section { Id = 13, Name = "Армани", Order = 5, ParentId = 7 },
-              new Section { Id = 14, Name = "Prada", Order = 6, ParentId = 7 },
-              new Section { Id = 15, Name = "Дольче и Габбана", Order = 7, ParentId = 7 },
-              new Section { Id = 16, Name = "Шанель", Order = 8, ParentId = 7 },
-              new Section { Id = 17, Name = "Гуччи", Order = 9, ParentId = 7 },
-              new Section { Id = 18, Name = "Для женщин", Order = 2 },
-              new Section { Id = 19, Name = "Fendi", Order = 0, ParentId = 18 },
-              new Section { Id = 20, Name = "Guess", Order = 1, ParentId = 18 },
-              new Section { Id = 21, Name = "Valentino", Order = 2, ParentId = 18 },
-              new Section { Id = 22, Name = "Dior", Order = 3, ParentId = 18 },
-              new Section { Id = 23, Name = "Versace", Order = 4, ParentId = 18 },
-              new Section { Id = 24, Name = "Для детей", Order = 3 },
-              new Section { Id = 25, Name = "Мода", Order = 4 },
-              new Section { Id = 26, Name = "Для дома", Order = 5 },
-              new Section { Id = 27, Name = "Интерьер", Order = 6 },
-              new Section { Id = 28, Name = "Одежда", Order = 7 },
-              new Section { Id = 29, Name = "Сумки", Order = 8 },
-              new Section { Id = 30, Name = "Обувь", Order = 9 },
+            new Section { Id = 1, Name = "Спорт", Order = 0 },
+            new Section { Id = 2, Name = "Nike", Order = 0, ParentId = 1 },
+            new Section { Id = 3, Name = "Under Armour", Order = 1, ParentId = 1 },
+            new Section { Id = 4, Name = "Adidas", Order = 2, ParentId = 1 },
+            new Section { Id = 5, Name = "Puma", Order = 3, ParentId = 1 },
+            new Section { Id = 6, Name = "ASICS", Order = 4, ParentId = 1 },
+            new Section { Id = 7, Name = "Для мужчин", Order = 1 },
+            new Section { Id = 8, Name = "Fendi", Order = 0, ParentId = 7 },
+            new Section { Id = 9, Name = "Guess", Order = 1, ParentId = 7 },
+            new Section { Id = 10, Name = "Valentino", Order = 2, ParentId = 7 },
+            new Section { Id = 11, Name = "Диор", Order = 3, ParentId = 7 },
+            new Section { Id = 12, Name = "Версачи", Order = 4, ParentId = 7 },
+            new Section { Id = 13, Name = "Армани", Order = 5, ParentId = 7 },
+            new Section { Id = 14, Name = "Prada", Order = 6, ParentId = 7 },
+            new Section { Id = 15, Name = "Дольче и Габбана", Order = 7, ParentId = 7 },
+            new Section { Id = 16, Name = "Шанель", Order = 8, ParentId = 7 },
+            new Section { Id = 17, Name = "Гуччи", Order = 9, ParentId = 7 },
+            new Section { Id = 18, Name = "Для женщин", Order = 2 },
+            new Section { Id = 19, Name = "Fendi", Order = 0, ParentId = 18 },
+            new Section { Id = 20, Name = "Guess", Order = 1, ParentId = 18 },
+            new Section { Id = 21, Name = "Valentino", Order = 2, ParentId = 18 },
+            new Section { Id = 22, Name = "Dior", Order = 3, ParentId = 18 },
+            new Section { Id = 23, Name = "Versace", Order = 4, ParentId = 18 },
+            new Section { Id = 24, Name = "Для детей", Order = 3 },
+            new Section { Id = 25, Name = "Мода", Order = 4 },
+            new Section { Id = 26, Name = "Для дома", Order = 5 },
+            new Section { Id = 27, Name = "Интерьер", Order = 6 },
+            new Section { Id = 28, Name = "Одежда", Order = 7 },
+            new Section { Id = 29, Name = "Сумки", Order = 8 },
+            new Section { Id = 30, Name = "Обувь", Order = 9 },
         };
         private readonly IEnumerable<Brand> _Brands = new[]
         {
@@ -69,12 +71,15 @@ namespace WebApplication1.Services
             new Product { Id = 12, Name = "Летний костюм", Price = 1025, ImageUrl = "product12.jpg", Order = 11, SectionId = 25, BrandId = 3 },
         };
 
+        #endregion
+
         public IEnumerable<Section> GetSections() => _Sections;
 
         public IEnumerable<Brand> GetBrands() => _Brands;
+
         public IEnumerable<Product> GetProducts(ProductFilter productFilter = null)
         {
-            var query = _Products;
+            IEnumerable<Product> query = _Products;
             if (productFilter?.SectionId is { } sectionId) 
                 query = query.Where(p => p.SectionId == sectionId);
             if (productFilter?.BrandId is { } brandId)

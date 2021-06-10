e5105ea8e49f42c9d91521231098bdabf77a8417
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-06-10 09:46:23 +0400

    Модели пользователей и роли пользователей

---
+++ WebApplication.Domain/Identity/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApplication.Domain.Identity
+{
+    public class User : IdentityUser
+    {
+        public const string Administrator = "sa";
+        public const string DefaultAdministratorPassword = "123";
+    }
+}

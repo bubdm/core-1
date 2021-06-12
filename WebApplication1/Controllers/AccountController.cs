2f588a21da5c7a9d1f3e12fc75d5600e35cae5ba
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-06-12 07:41:25 +0400

    Добавление регистрации, логина и поправка шаблона

--- WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/Controllers/AccountController.cs
@@ -1,21 +1,79 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Domain.Identity;
+using WebApplication1.ViewModel;
 
 namespace WebApplication1.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Register()
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Register() => View(new RegisterUserViewModel());
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var user = new User
+            {
+                UserName = model.UserName,
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in result.Errors) 
+                ModelState.AddModelError("", error.Description);
+
             return View();
         }
-        public IActionResult Login()
+
+        public IActionResult Login(string returnUrl) => View(new LoginViewModel {ReturnUrl = returnUrl});
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
         {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var result =
+                await _signInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe,
+#if DEBUG
+                    false
+#else
+                    true
+#endif
+                );
+
+            if (result.Succeeded)
+                return LocalRedirect(model.ReturnUrl);
+
+            ModelState.AddModelError("", "Ошибка в имени пользователя, либо в пароле");
+
             return View();
         }
-        public IActionResult Logout()
+
+        public async Task<IActionResult> Logout()
         {
+            await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
+
         public IActionResult AccessDenied()
         {
             return View();
---
+++ WebApplication1/ViewModel/RegisterUserViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.ViewModel
+{
+    public class RegisterUserViewModel
+    {
+        [Required]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Подтверждение пароля")]
+        [DataType((DataType.Password))]
+        [Compare(nameof(Password))]
+        public string PasswordConfirm { get; set; }
+    }
+}

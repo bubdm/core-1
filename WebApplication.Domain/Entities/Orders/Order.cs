5f8fe75198d1d0ad02fa4d7b43179868d816e692
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-06-19 14:57:58 +0400

    Сущности заказов в базу данных +

---
+++ WebApplication.Domain/Entities/Orders/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using WebApplication.Domain.Entities.Base;
+using WebApplication.Domain.Identity;
+
+namespace WebApplication.Domain.Entities.Orders
+{
+    public class Order : NamedEntity
+    {
+        [Required]
+        public User User { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Phone { get; set; }
+
+        [Required, MaxLength(600)]
+        public string Address { get; set; }
+
+        public DateTime DateTime { get; set; } = DateTime.Now;
+
+        public ICollection<OrderItem> Items { get; set; } = new List<OrderItem>();
+    }
+}
--- WebApplication1.Dal/Context/Application1DB.cs
+++ WebApplication1.Dal/Context/Application1DB.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using WebApplication.Domain.Entities;
+using WebApplication.Domain.Entities.Orders;
 using WebApplication.Domain.Identity;
 
 namespace WebApplication1.Dal.Context
@@ -11,6 +12,30 @@ namespace WebApplication1.Dal.Context
         public DbSet<Section> Sections { get; set; }
         public DbSet<Brand> Brands { get; set; }
         public DbSet<Person> Persons { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
         public Application1DB(DbContextOptions<Application1DB> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Order>()
+                .HasMany(order => order.Items)
+                .WithOne(item => item.Order)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<User>()
+                .HasMany<Order>()
+                .WithOne(order => order.User)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<OrderItem>()
+                .HasOne(item => item.Product)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }

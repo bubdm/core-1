f698289bbdeb3642df76f07e9dcc0a3b0c1fa52d
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-06-24 21:23:17 +0400

    Доработка стартового

--- Services/WebApplication1.WebAPI/Program.cs
+++ Services/WebApplication1.WebAPI/Program.cs
@@ -5,16 +5,13 @@ namespace WebApplication1.WebAPI
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => webBuilder
+                    .UseStartup<Startup>()
+                )
+            ;
     }
 }

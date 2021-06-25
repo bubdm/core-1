25e6f2dbc00aaf4c41ff0a14034094708faa79e9
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-06-25 20:06:02 +0400

    Не работает засылка данных :-(

---
+++ UI/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using WebApplication1.WebAPI.Clients.Values;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static string __WebAPI = "http://localhost:5001";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Тестирование изменений данных через сервис");
+            
+            var client = new ValuesClient(new HttpClient{BaseAddress = new Uri(__WebAPI)});
+
+            PrintData(client, "Элементы изначально:");
+
+            Console.WriteLine("Нажмите любую кнопку и будет добавлен один дополнительный элемент");
+            Console.ReadKey();
+
+            client.Add("New Item");
+            
+            PrintData(client, "Элементы сейчас:");
+
+            Console.WriteLine("Для завершения нажмите любую кнопку ...");
+            Console.ReadLine();
+        }
+
+        #region Вспомогательное
+
+        private static void PrintData(ValuesClient client, string message)
+        {
+            Console.WriteLine(message);
+            foreach (var item in client.GetAll())
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+
+        #endregion
+    }
+}

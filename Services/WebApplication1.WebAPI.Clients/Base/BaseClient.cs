64d69a0d01b2a3ff5f2d419e15ee9d7317489ba1
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-06-29 00:02:58 +0400

    Базовые клиенты доступа к WebAPI

--- Services/WebApplication1.WebAPI.Clients/Base/BaseClient.cs
+++ Services/WebApplication1.WebAPI.Clients/Base/BaseClient.cs
@@ -6,12 +6,33 @@ namespace WebApplication1.WebAPI.Clients.Base
 {
     public abstract class BaseClient
     {
-        protected readonly HttpClient _Client;
-        protected readonly string _Address;
+        protected readonly HttpClient _client;
+        protected readonly string _address;
         public BaseClient(HttpClient client, string address)
         {
-            _Client = client;
-            _Address = address;
+            _client = client;
+            _address = address;
+        }
+        protected async Task<T> GetAsync<T>(string url)
+        {
+            var response = await _client.GetAsync(url).ConfigureAwait(false);
+            return await response
+                .EnsureSuccessStatusCode().Content.ReadFromJsonAsync<T>().ConfigureAwait(false);
+        }
+        protected async Task<HttpResponseMessage> PostAsync<T>(string url, T item)
+        {
+            var response = await _client.PostAsJsonAsync(url, item).ConfigureAwait(false);
+            return response.EnsureSuccessStatusCode();
+        }
+        protected async Task<HttpResponseMessage> PutAsync<T>(string url, T item)
+        {
+            var response = await _client.PutAsJsonAsync(url, item).ConfigureAwait(false);
+            return response.EnsureSuccessStatusCode();
+        }
+        protected async Task<HttpResponseMessage> DeleteAsync(string url)
+        {
+            var response = await _client.DeleteAsync(url).ConfigureAwait(false);
+            return response;
         }
     }
 }
---
+++ Services/WebApplication1.WebAPI.Clients/Base/BaseSyncClient.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+
+namespace WebApplication1.WebAPI.Clients.Base
+{
+    class BaseSyncClient : BaseClient
+    {
+        public BaseSyncClient(HttpClient client, string address) : base(client, address) { }
+        protected T Get<T>(string url) => GetAsync<T>(url).Result;
+        protected HttpResponseMessage Post<T>(string url, T item) => PostAsync(url, item).Result;
+        protected HttpResponseMessage Put<T>(string url, T item) => PutAsync(url, item).Result;
+        protected HttpResponseMessage Delete<T>(string url) => DeleteAsync(url).Result;
+    }
+}

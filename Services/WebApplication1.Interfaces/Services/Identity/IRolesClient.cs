641123bc4835e7eeb734a41da0fd931d44a851c0
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-03 02:42:07 +0400

    Интерфейсы сервисов системы Identity

---
+++ Services/WebApplication1.Interfaces/Services/Identity/IRolesClient.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebApplication1.Domain.Identity;
+
+namespace WebApplication1.Interfaces.Services.Identity
+{
+    public interface IRolesClient : IRoleStore<Role>
+    {
+    }
+}

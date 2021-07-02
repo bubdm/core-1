b8063d8178fc97eb91a9ad48e44e9fa31d48dabb
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-03 02:55:36 +0400

    Базовые опеределения контроллеров Identity

---
+++ Services/WebApplication1.WebAPI/Controllers/Identity/RolesController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Dal.Context;
+using WebApplication1.Domain.Identity;
+using WebApplication1.Interfaces.Adresses;
+
+namespace WebApplication1.WebAPI.Controllers.Identity
+{
+    [Route(WebAPIInfo.Roles), ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly RoleStore<Role> _roleStore;
+        public RolesController(Application1Context context)
+        {
+            _roleStore = new RoleStore<Role>(context);
+        }
+
+        [HttpGet("all")]
+        public async Task<IEnumerable<Role>> GetAllRoles() => 
+            await _roleStore.Roles.ToArrayAsync();
+
+
+
+    }
+}
---
+++ Services/WebApplication1.WebAPI/Controllers/Identity/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Dal.Context;
+using WebApplication1.Domain.Identity;
+using WebApplication1.Interfaces.Adresses;
+
+namespace WebApplication1.WebAPI.Controllers.Identity
+{
+    [Route(WebAPIInfo.Users), ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserStore<User, Role, Application1Context> _userStore;
+        public UsersController(Application1Context context)
+        {
+            _userStore = new UserStore<User, Role, Application1Context>(context);
+        }
+
+        [HttpGet("all")]
+        public async Task<IEnumerable<User>> GetAllUsers() => 
+            await _userStore.Users.ToArrayAsync();
+
+
+
+    }
+}

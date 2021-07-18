24205ffb1406960f359ef469c16be1e1b071c750
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-18 08:12:26 +0400

    Исправление ошибки

--- Common/WebApplication1.Domain/Entities/Cart.cs
+++ Common/WebApplication1.Domain/Entities/Cart.cs
@@ -6,6 +6,6 @@ namespace WebApplication1.Domain.Entities
     public class Cart
     {
         public ICollection<CartItem> Items { get; set; } = new List<CartItem>();
-        public int ItemsCount => Items?.Sum(i => i.Quantity) + 5 ?? 0;
+        public int ItemsCount => Items?.Sum(i => i.Quantity) ?? 0;
     }
 }

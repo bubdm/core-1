b7da31f69bd039999691438c043b33f1f7c0da3a
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-18 07:21:12 +0400

    Тесты для корзины +

--- Tests/WebApplication1.Services.Tests/Services/CartServiceTests.cs
+++ Tests/WebApplication1.Services.Tests/Services/CartServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using Moq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -98,5 +99,83 @@ namespace WebApplication1.Services.Tests.Services
             Assert.AreEqual(expectedTotalPrice, actualTotalPrice);
         }
 
+        [TestMethod]
+        public void CartService_Add_Correct()
+        {
+            _cart.Items.Clear();
+            const int expId = 1;
+            const int expCount = 1;
+
+            _cartService.Add(expId);
+
+            Assert.AreEqual(expCount, _cart.ItemsCount);
+            Assert.AreEqual(expCount, _cart.Items.Count);
+            Assert.AreEqual(expId, _cart.Items.First().ProductId);
+        }
+
+        [TestMethod]
+        public void CartService_Remove_Correct()
+        {
+            const int removedId = 1;
+            const int expFirstProductId = 2;
+            const int expCount = 2;
+            const int expItemsCount = 5;
+
+            _cartService.Remove(removedId);
+
+            Assert.AreEqual(expCount, _cart.Items.Count);
+            Assert.AreEqual(expItemsCount, _cart.ItemsCount);
+            Assert.AreEqual(expFirstProductId, _cart.Items.First().ProductId);
+        }
+
+        [TestMethod]
+        public void CartService_Clear_Correct()
+        {
+            _cartService.Clear();
+
+            Assert.AreEqual(0, _cart.Items.Count);
+        }
+
+        [TestMethod]
+        public void CartService_Minus_Correct()
+        {
+            const int minusId = 2;
+            const int expCountItem = 1;
+            const int expCount = 3;
+            const int expItemsCount = 5;
+
+            _cartService.Minus(minusId);
+
+            Assert.AreEqual(expCount, _cart.Items.Count);
+            Assert.AreEqual(expItemsCount, _cart.ItemsCount);
+            var items = _cart.Items.ToImmutableArray();
+            Assert.AreEqual(minusId, items[1].ProductId);
+            Assert.AreEqual(expCountItem, items[1].Quantity);
+        }
+
+        [TestMethod]
+        public void CartService_Remove_Item_When_Decrement_to_0()
+        {
+            const int minusId = 1;
+            const int expCount = 2;
+            const int expItemsCount = 5;
+
+            _cartService.Minus(minusId);
+
+            Assert.AreEqual(expCount, _cart.Items.Count);
+            Assert.AreEqual(expItemsCount, _cart.ItemsCount);
+        }
+
+        [TestMethod]
+        public void CartService_GetWebModel_WorkCorrect()
+        {
+            const int expItemsCount = 6;
+            const decimal expFirstProductPrice = 1.1m;
+
+            var result = _cartService.GetWebModel();
+
+            Assert.AreEqual(expItemsCount, result.ItemsCount);
+            Assert.AreEqual(expFirstProductPrice, result.Items.First().Product.Price);
+        }
     }
 }

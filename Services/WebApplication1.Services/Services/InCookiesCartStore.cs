72c9c39c2297f9332010943b1bbdd4f8fd0215a7
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-18 04:53:16 +0400

    Сервис хранения корзины в куках

---
+++ Services/WebApplication1.Services/Services/InCookiesCartStore.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using WebApplication1.Domain.Entities;
+using WebApplication1.Interfaces.Services;
+
+namespace WebApplication1.Services.Services
+{
+    public class InCookiesCartStore : ICartStore
+    {
+        private readonly IHttpContextAccessor _ContextAccessor;
+        private readonly string _cartName;
+
+        public Cart Cart
+        {
+            get
+            {
+                var context = _ContextAccessor.HttpContext;
+                if (!context!.Request.Cookies.ContainsKey(_cartName))
+                {
+                    var cart = new Cart();
+                    context.Response.Cookies.Append(_cartName, JsonConvert.SerializeObject(cart));
+                    return cart;
+                }
+                return JsonConvert.DeserializeObject<Cart>(context.Request.Cookies[_cartName]);
+            }
+            set => _ContextAccessor.HttpContext!.Response.Cookies.Append(_cartName, JsonConvert.SerializeObject(value));
+        }
+
+        public InCookiesCartStore(IHttpContextAccessor contextAccessor)
+        {
+            _ContextAccessor = contextAccessor;
+
+            var user = _ContextAccessor.HttpContext!.User;
+            var userName = user!.Identity!.IsAuthenticated ? $"-{user.Identity.Name}" : null;
+
+            _cartName = $"WebStore.Cart{userName}";
+        }
+    }
+}

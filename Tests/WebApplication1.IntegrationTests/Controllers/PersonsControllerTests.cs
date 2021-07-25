2db3a4835c09932d4a0e6a73419dfeecce8fba09
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-25 20:07:27 +0400

    Интеграционный тест приложение + веб апи с использованием драйвера

--- Tests/WebApplication1.IntegrationTests/Controllers/PersonsControllerTests.cs
+++ Tests/WebApplication1.IntegrationTests/Controllers/PersonsControllerTests.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Security.AccessControl;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using Moq.Protected;
 using WebApplication1.Domain.Entities;
 using WebApplication1.Interfaces.Services;
+using WebApplication1.WebAPI.Clients.Persons;
 
 namespace WebApplication1.IntegrationTests.Controllers
 {
@@ -50,6 +56,80 @@ namespace WebApplication1.IntegrationTests.Controllers
 
             personsDataMock.Verify(_ => _.GetAll());
             personsDataMock.Verify();
+        }
+        
+        [TestMethod]
+        public void Index_SendRequest_HttpDriver_ShouldReturnView()
+        {
+            const int expectedCount = 3;
+            var webAPIPersonsControllerDriver = new WebAPI_HttpDriver_PersonsController_GetAll();
+            Mock<IPersonsData> personsDataMock = null;
+            WebApplicationFactory<Startup> webHost = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(IPersonsData));
+                        services.Remove(descriptor);
+                        var mockMessageHandler = new Mock<HttpMessageHandler>();
+                        mockMessageHandler.Protected()
+                            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                            .ReturnsAsync(() =>
+                            {
+                                return webAPIPersonsControllerDriver.GetAll(expectedCount);
+                            });
+                        var client = new PersonsClient(new HttpClient(mockMessageHandler.Object) { BaseAddress = new Uri("http://localost/") });
+
+                        services.AddTransient<IPersonsData>(_ => client);
+                    });
+                });
+            var httpClient = webHost.CreateClient();
+
+            var response = httpClient.GetAsync("persons").Result;
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.IsTrue(webAPIPersonsControllerDriver.BeCalled);
+        }
+
+        /// <summary> Драйвер для контроллера веб апи </summary>
+        public class WebAPI_HttpDriver_PersonsController_GetAll
+        {
+            public bool BeCalled { get; set; } = false;
+            public HttpResponseMessage GetAll(int expectedCount = 3)
+            {
+                WebApplicationFactory<WebAPI.Startup> webHost = new WebApplicationFactory<WebAPI.Startup>()
+                    .WithWebHostBuilder(builder =>
+                    {
+                        builder.ConfigureTestServices(services =>
+                        {
+                            var descriptor = services
+                                .SingleOrDefault(s => s.ServiceType == typeof(IPersonsData));
+                            services.Remove(descriptor);
+                            var personsDataMock = new Mock<IPersonsData>();
+                            personsDataMock
+                                .Setup(_ => _.GetAll())
+                                .Returns(
+                                    () =>
+                                    {
+                                        BeCalled = true;
+                                        return Enumerable.Range(1, expectedCount).Select(i => new Person
+                                        {
+                                            Id = i,
+                                            LastName = "Ivanov",
+                                            FirstName = "Ivan",
+                                            Patronymic = "Ivanovich",
+                                        });
+                                    }
+                                );
+                            services.AddTransient(_ => personsDataMock.Object);
+                        });
+                    });
+                var httpClient = webHost.CreateClient();
+
+                var response = httpClient.GetAsync("api/persons").Result;
+
+                return response;
+            }
         }
     }
 }
--- Tests/WebApplication1.WebAPI.IntegrationTests/Controllers/PersonsControllerTests.cs
+++ Tests/WebApplication1.WebAPI.IntegrationTests/Controllers/PersonsControllerTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -24,6 +25,8 @@ namespace WebApplication1.WebAPI.IntegrationTests.Controllers
         [TestMethod]
         public void GetAll_SendRequest_ReplaceService_ShouldReturnOk()
         {
+            const int expectedId = 1;
+            const int expectedCount = 3;
             Mock<IPersonsData> personsDataMock = null;
             WebApplicationFactory<Startup> webHost = new WebApplicationFactory<Startup>()
                 .WithWebHostBuilder(builder =>
@@ -37,7 +40,7 @@ namespace WebApplication1.WebAPI.IntegrationTests.Controllers
                         personsDataMock
                             .Setup(_ => _.GetAll())
                             .Returns(
-                                Enumerable.Range(1, 3).Select(i => new Person
+                                Enumerable.Range(1, expectedCount).Select(i => new Person
                                 {
                                     Id = i,
                                     LastName = "Ivanov",
@@ -53,12 +56,63 @@ namespace WebApplication1.WebAPI.IntegrationTests.Controllers
             var response = httpClient.GetAsync("api/persons").Result;
 
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            var persons = response.Content.ReadFromJsonAsync(typeof(IEnumerable<Person>)).Result;
+            Assert.IsInstanceOfType(persons, typeof(IEnumerable<Person>));
+            var personsList = (IEnumerable<Person>) persons;
+            Assert.AreEqual(expectedId, personsList.FirstOrDefault().Id);
+            Assert.AreEqual(expectedCount, personsList.Count());
             personsDataMock.Verify(_ => _.GetAll());
             personsDataMock.Verify();
         }
 
         [TestMethod]
-        public void GetById_SendRequest_ReplaceContext_ShouldReturnOk()
+        public void GetAll_SendRequest_ReplaceDatabaseContext_ShouldReturnOk()
+        {
+            const int expectedId = 1;
+            const int expectedCount = 3;
+            WebApplicationFactory<Startup> webHost = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureTestServices(services =>
+                    {
+                        var descriptor = services
+                            .SingleOrDefault(_ => _.ServiceType == typeof(DbContextOptions<Application1Context>));
+                        services.Remove(descriptor);
+                        services.AddDbContext<Application1Context>(options =>
+                        {
+                            options.UseInMemoryDatabase(nameof(Application1Context));
+                        });
+                        var descriptorInitializer = services
+                            .SingleOrDefault(_ => _.ServiceType == typeof(WebStoreDBInitializer));
+                        services.Remove(descriptorInitializer);
+                        services.AddTransient(_ => Mock.Of<WebStoreDBInitializer>());
+                    });
+                });
+            var testContext = webHost
+                .Services.CreateScope().ServiceProvider.GetService<Application1Context>();
+            testContext!.Persons.AddRange(
+                Enumerable.Range(1, expectedCount).Select(i => new Person
+                {
+                    Id = i,
+                    LastName = "Ivanov",
+                    FirstName = "Ivan",
+                    Patronymic = "Ivanovich",
+                }));
+            testContext!.SaveChanges();
+            HttpClient httpClient = webHost.CreateClient();
+
+            var response = httpClient.GetAsync("api/persons").Result;
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            var persons = response.Content.ReadFromJsonAsync(typeof(IEnumerable<Person>)).Result;
+            Assert.IsInstanceOfType(persons, typeof(IEnumerable<Person>));
+            var personsList = (IEnumerable<Person>) persons;
+            Assert.AreEqual(expectedId, personsList.FirstOrDefault().Id);
+            Assert.AreEqual(expectedCount, personsList.Count());
+        }
+
+        [TestMethod]
+        public void GetById_SendRequest_ReplaceDatabaseContext_ShouldReturnOk()
         {
             const int expectedId = 1;
             WebApplicationFactory<Startup> webHost = new WebApplicationFactory<Startup>()
@@ -71,7 +125,7 @@ namespace WebApplication1.WebAPI.IntegrationTests.Controllers
                         services.Remove(descriptor);
                         services.AddDbContext<Application1Context>(options =>
                         {
-                            options.UseInMemoryDatabase("InMemoryDbForTesting");
+                            options.UseInMemoryDatabase(nameof(Application1Context));
                         });
                         var descriptorInitializer = services
                             .SingleOrDefault(_ => _.ServiceType == typeof(WebStoreDBInitializer));
@@ -95,8 +149,10 @@ namespace WebApplication1.WebAPI.IntegrationTests.Controllers
             var response = httpClient.GetAsync($"api/persons/{expectedId}").Result;
 
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            var person = response.Content.ReadFromJsonAsync(typeof(Person)).Result;
+            Assert.IsInstanceOfType(person, typeof(Person));
+            var personOne = (Person) person;
+            Assert.AreEqual(expectedId, personOne.Id);
         }
-
-
     }
 }

a00ac177818156cf570f1bed772bb5dfb60e828a
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-26 13:04:24 +0400

    Инфраструктура таг хелпера

--- UI/WebApplication1/TagHelpers/ActiveRouteTagHelper.cs
+++ UI/WebApplication1/TagHelpers/ActiveRouteTagHelper.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +11,39 @@ namespace WebApplication1.TagHelpers
     [HtmlTargetElement(Attributes = attributeName)]
     public class ActiveRoute : TagHelper
     {
-        private const string attributeName = "my-active-route";
+        private const string attributeName = "active-route";
+        
+        [HtmlAttributeName("asp-controller")]
+        public string Controller { get; set; }
+
+        [HtmlAttributeName("asp-action")]
+        public string Action { get; set; }
+
+        [HtmlAttributeName("asp-all-route-data", DictionaryAttributePrefix = "asp-route-")]
+        public Dictionary<string, string> RouteValues { get; set; } = new (StringComparer.OrdinalIgnoreCase);
+
+        [ViewContext, HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+        
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
+            if (IsActive())
+                MakeActive(output);
+
+
             output.Attributes.RemoveAll(attributeName);
+        }
+
+        private bool IsActive()
+        {
+
+
+            return true;
+        }
+
+        private static void MakeActive(TagHelperOutput output)
+        {
+
         }
     }
 }

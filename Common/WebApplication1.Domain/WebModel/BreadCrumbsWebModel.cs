02ca7c77bcbeaf71011a8e5f38892991ed4fe240
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-26 17:16:35 +0400

    Заготовка хлебных крошек отображается

---
+++ Common/WebApplication1.Domain/WebModel/BreadCrumbsWebModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApplication1.Domain.Entities;
+
+namespace WebApplication1.Domain.WebModel
+{
+    /// <summary> Веб модель для отображения хлебных крошек </summary>
+    public class BreadCrumbsWebModel
+    {
+        public Section Section { get; set; }
+        public Brand Brand { get; set; }
+        public string Product { get; set; }
+    }
+}

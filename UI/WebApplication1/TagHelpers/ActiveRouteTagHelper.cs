d06538731161084536499318b5e297f4c240e8c1
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-26 12:53:49 +0400

    Основа таг-хелпера

---
+++ UI/WebApplication1/TagHelpers/ActiveRouteTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.TagHelpers
+{
+    [HtmlTargetElement(Attributes = attributeName)]
+    public class ActiveRoute : TagHelper
+    {
+        private const string attributeName = "my-active-route";
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.RemoveAll(attributeName);
+        }
+    }
+}

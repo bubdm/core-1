f35c4daf52cd85851ad74ebcf249d4d4519793d1
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-27 21:17:41 +0400

    Сорение хлебными крошками +

--- Common/WebApplication1.Domain/WebModel/BreadCrumbsWebModel.cs
+++ Common/WebApplication1.Domain/WebModel/BreadCrumbsWebModel.cs
@@ -5,6 +5,8 @@ namespace WebApplication1.Domain.WebModel
     /// <summary> Веб модель для отображения хлебных крошек </summary>
     public class BreadCrumbsWebModel
     {
+        public string ControllerText { get; set; }
+        public string Controller { get; set; }
         public Section Section { get; set; }
         public Brand Brand { get; set; }
         public string Product { get; set; }
--- UI/WebApplication1/Components/BreadCrumbsViewComponent.cs
+++ UI/WebApplication1/Components/BreadCrumbsViewComponent.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Domain.WebModel;
 using WebApplication1.Interfaces.Services;
 
@@ -16,6 +17,21 @@ namespace WebApplication1.Components
         public IViewComponentResult Invoke()
         {
             var model = new BreadCrumbsWebModel();
+            if (ViewContext.RouteData.Values["controller"] is string controller && controller != "Home")
+            {
+                model.Controller = controller;
+                model.ControllerText = controller switch
+                {
+                    "Account" => "Авторизация",
+                    "Cart" => "Корзина",
+                    "Catalog" => "Каталог",
+                    "Home" => "Главная",
+                    "Persons" => "Работники",
+                    "UserProfile" => "Профиль",
+                    "WebAPI" => "WebAPI",
+                    _ => throw new ArgumentOutOfRangeException($"Непостумый параметр {nameof(controller)} значение = {controller}, неизвестное имя контроллера"),
+                };
+            }
             if (int.TryParse(Request.Query["SectionId"], out var sectionId))
             {
                 model.Section = _productData.GetSection(sectionId);

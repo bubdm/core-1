a106dd97852d3fd773890aa3ebac429b14e93991
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-28 09:56:58 +0400

    Создание визуального компонента корзины для асинхронщины

---
+++ UI/WebApplication1/Components/CartViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Interfaces.Services;
+
+namespace WebApplication1.Components
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly ICartService _cartService;
+        public CartViewComponent(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.CartProductsCount = _cartService.GetWebModel().ItemsCount;
+            return View();
+        }
+    }
+}

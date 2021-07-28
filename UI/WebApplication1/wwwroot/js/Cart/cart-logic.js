f8b8cfb3668b0689a2716659031af60fa9bd576d
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-28 12:12:24 +0400

    Работает AJAX увеличение товаров в корзине

--- UI/WebApplication1/wwwroot/js/Cart/cart-logic.js
+++ UI/WebApplication1/wwwroot/js/Cart/cart-logic.js
@@ -4,18 +4,20 @@
         addToCartLink: "",
         minusFromCartLink: "",
         removeFromCartLink: "",
+        clearCartLink: "",
     },
 
     init: function (properties) {
         $.extend(Cart._properties, properties);
 
         $(".add-to-cart").click(Cart.addToCart);
+        $(".cart_quantity_up").click(Cart.incrementIremInCart);
     },
 
     addToCart: function (event) {
         event.preventDefault();
 
-        var button = $(this);
+        let button = $(this);
         const id = button.data("id");
 
         $.get(Cart._properties.addToCartLink + "/" + id)
@@ -25,6 +27,26 @@
             })
             .fail(function () {
                 console.log("addToCart fail");
+            });
+    },
+
+    incrementIremInCart: function () {
+        event.preventDefault();
+
+        let button = $(this);
+        const id = button.data("id");
+
+        var tr = button.closest("tr");
+
+        $.get(Cart._properties.addToCartLink + "/" + id)
+            .done(function () {
+                let count = parseInt($(".cart_quantity_input", tr).val());
+                $(".cart_quantity_input", tr).val(count + 1);
+                Cart.refreshPrice(tr);
+                Cart.refreshCartView();
+            })
+            .fail(function () {
+                console.log("incrementIremInCart fail");
             });
     },
 
@@ -44,5 +66,31 @@
             .fail(function () {
                 console.log("refreshCartView fail");
             });
+    },
+
+    refreshPrice: function (tr) {
+        let count = parseInt($(".cart_quantity_input", tr).val());
+        let price = parseInt($(".cart_price", tr).data("price"));
+
+        let sumPrice = price * count;
+        let value = sumPrice.toLocaleString("ru-RU", { style: "currency", currency: "RUB" });
+
+        let cartTotalPrice = $(".cart_total_price", tr);
+        cartTotalPrice.data("price", sumPrice);
+        cartTotalPrice.html(value);
+
+        Cart.refreshTotalPrice();
+    },
+
+    refreshTotalPrice: function () {
+        let totalPrice = 0;
+
+        $(".cart_total_price").each(function () {
+            let price = parseFloat($(this).data("price"));
+            totalPrice += price;
+        });
+        let value = totalPrice.toLocaleString("ru-RU", { style: "currency", currency: "RUB" });
+
+        $("#total-order-price").html(value);
     },
 }

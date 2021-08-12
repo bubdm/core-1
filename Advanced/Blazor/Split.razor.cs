9828fc24a7f1986349261e7ad3a97271b5be6e5a
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-08-12 05:47:52 +0400

    Сортировка в отделенном классе + компоненте работает

---
+++ Advanced/Blazor/Split.razor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Advanced.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Advanced.Blazor
+{
+    public partial class Split
+    {
+        [Inject]
+        public DataContext Context { get; set; }
+
+        public bool Ascending { get; set; } = false;
+
+        public IEnumerable<string> Names => Context.Persons.Select(p => p.FirstName);
+    }
+}
